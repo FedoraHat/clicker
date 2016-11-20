e1ea75f94f25fee50220128698eb81ba8a992d2c
Paulo <elie9alb@gmail.com>
2016-11-20 00:04:46 -0300

    Fix debug.log

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var hugs = 0;
+var hugs = 0; //declarando a variável de moeda
 
-function hugClick(n){
+function hugClick(n){ //aumentando hugs a cada clique
 	hugs = hugs + n;
 	document.getElementById("hugs").innerHTML = hugs;
 };
